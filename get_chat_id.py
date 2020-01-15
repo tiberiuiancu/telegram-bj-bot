bf145f0b94315ddb33152712fa408e4b1e644de2
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2020-01-15 12:29:13 +0100

    updated to support private chats

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,4 +1,7 @@
 from api import update_all
 
 for message in update_all():
-    print(message.chat.title + ': ' + str(message.chat.id))
+    if message.chat.title is not None:
+        print(message.chat.title + ': ' + str(message.chat.id))
+    else:
+        print('Private message from ' + str(message.chat.id))
